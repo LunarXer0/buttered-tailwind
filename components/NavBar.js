776e3c432ab2f91daeade9abdf84bb912b6a79db
Victor Rivera <verm2405@gmail.com>
2018-12-22 01:10:14 -0500

    Finally won the battle and have the nav bar looking like I want

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,8 +1,9 @@
 const NavBar = () => (
-  <div className="p-2 bg-yellow-dark rounded">
-    <h1 className="text-white font-sans text-center uppercase leading-normal">
+  <div className="p-2 bg-yellow-dark rounded flex items-center justify-between">
+    <h1 className="text-white inline-block font-sans text-left uppercase leading-normal">
       Buttered
     </h1>
+    <input className="" placeholder="Search Movie" />
   </div>
 );
 
