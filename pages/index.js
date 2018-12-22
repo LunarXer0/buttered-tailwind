9176284642fe6c050a493526da64b7ab1cebc1bf
Victor Rivera <verm2405@gmail.com>
2018-12-21 20:10:23 -0500

    Removed Hero Component from index

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,10 @@ import "../styles/index.css";
 import NavBar from "../components/NavBar";
 import ProfileHeader from "../components/ProfileHeader";
 import MovieGrid from "../components/MovieGrid";
-import Hero from "../components/Hero";
 
 const Profile = () => (
   <React.Fragment>
     <NavBar />
-    <Hero />
     <div className="p-3 z-50">
       <ProfileHeader />
       <MovieGrid />
