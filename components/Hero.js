da35b6864c02b23b2e76275dfc7d1450851528ca
Victor Rivera <verm2405@gmail.com>
2018-12-21 20:00:20 -0500

    Changes Hero component to use the image as the div background. Not as an image element. This allows to be able to completely cover the div

--- components/Hero.js
+++ components/Hero.js
@@ -3,25 +3,26 @@ import { getRandomBackDrop } from "../redux/actions/actions";
 
 export default class Hero extends Component {
   state = {
-    backdropPath: null
+    tmdbPosterPath: null
   };
   async componentDidMount() {
     const BACKDROP_PATH = await getRandomBackDrop();
-    this.setState({ backdropPath: BACKDROP_PATH });
+    const TMDB_POSTER_PATH = `https://image.tmdb.org/t/p/w1280${BACKDROP_PATH}`;
+    this.setState({ tmdbPosterPath: TMDB_POSTER_PATH });
   }
   render(poster) {
-    const { backdropPath } = this.state;
+    const { tmdbPosterPath } = this.state;
     return (
-      <div className="w-full z-0 text-center">
-        {backdropPath && (
-          <img
-            src={`https://image.tmdb.org/t/p/original${
-              this.state.backdropPath
-            }`}
-            alt="poster"
+      <React.Fragment>
+        {tmdbPosterPath && (
+          <div
+            className="w-full z-0 text-center h-64"
+            style={{
+              backgroundImage: `url(${tmdbPosterPath})`
+            }}
           />
         )}
-      </div>
+      </React.Fragment>
     );
   }
 }
