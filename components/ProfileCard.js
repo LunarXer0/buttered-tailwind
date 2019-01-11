f74e0fe34d19023c716c3f1da768f0af97d0d818
Victor Rivera <verm2405@gmail.com>
2019-01-10 22:36:40 -0500

    Profile Card now this displays total number of movies logged

--- components/ProfileCard.js
+++ components/ProfileCard.js
@@ -1,18 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { connect } from "react-redux";
 
-const ProfileCard = () => (
-  <div className="w-1/5 bg-twitter border border-solid border-twitter-grey rounded-lg flex mx-auto mt-10 flex-col items-center p-5 shadow-md">
-    <h3 className="text-white">Victor Rivera</h3>
-    <p className="my-2 text-twitter-font-grey">Orlando, Florida</p>
-    <div className="w-full text-twitter-font-grey flex justify-around">
-      <div className="my-2 flex flex-col items-center">
-        Movies <span className="text-twitter-red">50</span>
-      </div>
-      <div className="my-2 flex flex-col items-center">
-        Followers <span className="text-twitter-red">120</span>
+const ProfileCard = ({ movies }) => {
+  const [totalMovies, setTotalMovies] = useState(0);
+
+  useEffect(
+    () => {
+      setTotalMovies(movies.length);
+    },
+    [movies]
+  );
+
+  return (
+    <div className="w-1/5 bg-twitter border border-solid border-twitter-grey rounded-lg flex mx-auto mt-10 flex-col items-center p-5 shadow-md">
+      <h3 className="text-white">Victor Rivera</h3>
+      <p className="my-2 text-twitter-font-grey">Orlando, Florida</p>
+      <div className="w-full text-twitter-font-grey flex justify-around">
+        <div className="my-2 flex flex-col items-center">
+          Movies <span className="text-twitter-red">{totalMovies}</span>
+        </div>
+        <div className="my-2 flex flex-col items-center">
+          Followers <span className="text-twitter-red">120</span>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default ProfileCard;
+const mapStoreToProps = store => ({
+  movies: store.movies.movies
+});
+
+export default connect(mapStoreToProps)(ProfileCard);
